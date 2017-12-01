bf3363fafa2022784d63446af1ba2f8954d8d138
like0820 <33882742+like0820@users.noreply.github.com>
2017-12-01 17:23:13 +0900

    포토 데이터테이블
    ㅇ

--- src/com/nol2/model/tabledef/tabledef.sql
+++ src/com/nol2/model/tabledef/tabledef.sql
@@ -38,6 +38,7 @@ nocycle
 nocache;
 
 
+
 drop table prepass;
 create table prepass
 (
@@ -201,3 +202,8 @@ references customer(cusno);
 alter table prepass
 add constraint ride_rideno_fk foreign key (rideno)
 references ride(rideno);
+
+
+alter table ticket add (tphoto varchar2 (30));
+alter table event add (ephoto varchar2 (30));
+alter table ride add (rphoto varchar2(30));
